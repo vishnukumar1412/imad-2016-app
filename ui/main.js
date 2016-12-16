6176bf2250ee956f6580a5fe091a7564691a338f
vishnukumar1412 <vishnukumar1412@users.noreply.github.com>
2016-12-16 14:38:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
-
+  (function(d, s, id){
+    var js,
+        fjs = d.getElementsByTagName(s)[0],
+        p = (('https:' == d.location.protocol) ? 'https://' : 'http://'),
+        r = Math.floor(new Date().getTime() / 1000000);
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id; js.async=1;
+    js.src = p + "www.opinionstage.com/assets/loader.js?" + r;
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'os-widget-jssdk'));
+<div id="2407563" class="os_poll" data-path="/polls/2407563"></div>
